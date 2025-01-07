fb223d158097bd3ddc9b4b742f8c5ce83e69aee3
Tahatuna <tahatuna001@gmail.com>
2025-01-07 14:31:13 +0300

    Add fillFormInTextBox method

---
+++ src/test/java/org/seleniumdemo/pages/TextBoxPage.java
@@ -0,0 +1,38 @@
+package org.seleniumdemo.pages;
+
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.support.FindBy;
+import org.seleniumdemo.utilities.ConfigReader;
+
+public class TextBoxPage extends BasePage {
+    private static final Logger logger = LogManager.getLogger(TextBoxPage.class);
+
+    @FindBy(id = "userName")
+    private WebElement userName;
+    @FindBy(id = "userEmail")
+    private WebElement email;
+    @FindBy(id = "currentAddress")
+    private WebElement currentAddress;
+    @FindBy(id = "permanentAddress")
+    private WebElement permanentAddress;
+    @FindBy(id = "submit")
+    private WebElement submit;
+
+    public TextBoxPage(WebDriver driver) {
+        super(driver);
+        ConfigReader.loadProperties("src/test/resources/user.properties");
+        logger.info("Loaded user.properties file for TextBoxPage");
+    }
+
+    public void fillFormInTextBox() {
+        sendKeys(userName, ConfigReader.getProperty("userName"));
+        sendKeys(email, ConfigReader.getProperty("email"));
+        sendKeys(currentAddress, ConfigReader.getProperty("currentAddress"));
+        sendKeys(permanentAddress, ConfigReader.getProperty("permanentAddress"));
+        clickElement(submit);
+    }
+
+}
